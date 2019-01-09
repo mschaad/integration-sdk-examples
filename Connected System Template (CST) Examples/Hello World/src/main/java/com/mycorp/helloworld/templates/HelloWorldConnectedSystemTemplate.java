468e193a4b4d4e77da70f665f75c0e8ff9c75222
Matthew Schaad <matthew.schaad@appian.com>
2019-01-09 15:28:50 -0500

    dotdot

--- Connected System Template (CST) Examples/Hello World/src/main/java/com/mycorp/helloworld/templates/HelloWorldConnectedSystemTemplate.java
+++ Connected System Template (CST) Examples/Hello World/src/main/java/com/mycorp/helloworld/templates/HelloWorldConnectedSystemTemplate.java
@@ -5,7 +5,7 @@ import com.appian.connectedsystems.simplified.sdk.configuration.SimpleConfigurat
 import com.appian.connectedsystems.templateframework.sdk.ExecutionContext;
 import com.appian.connectedsystems.templateframework.sdk.TemplateId;
 
-@TemplateId(name="HelloWorldConnectedSystemTemplate")
+@TemplateId(name="../ConnectedSystemTemplate")
 public class HelloWorldConnectedSystemTemplate extends SimpleConnectedSystemTemplate {
 
   public static final String CS_PROP_KEY = "csProp";
